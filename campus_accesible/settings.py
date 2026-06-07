cc1f737dabd3db30de6baeabff0b467e1773f1f8
Juan Pablo <juanpablovega64@gmail.com>
2026-06-07 19:59:02 -0400

    Cambio a SQLite para despliegue

--- campus_accesible/settings.py
+++ campus_accesible/settings.py
@@ -75,12 +75,8 @@ WSGI_APPLICATION = 'campus_accesible.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'campus_db',         
-        'USER': 'root',   
-        'PASSWORD': 'admin',   
-        'HOST': '127.0.0.1',           
-        'PORT': '3306',               
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
